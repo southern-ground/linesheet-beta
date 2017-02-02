6be9333a9fd2a7f6ffa1185fa945beb064c33a60
Ferris Thomas <fsthomas@gmail.com>
2017-02-02 11:37:54 -0500

    Adds cookie writing and inventory fetching.

--- src/store.js
+++ src/store.js
@@ -8,20 +8,86 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import { createStore } from 'redux';
+import {createStore} from 'redux';
+import {
+    API,
+    API_GATEWAYS,
+    COOKIE_NAME,
+    GET_INVENTORY,
+    GET_INVENTORY_RESPONSE,
+    LOAD_COOKIE,
+    LOAD_COOKIE_RESPONSE
+} from './constants';
+import request from 'superagent';
+import Cookies from 'js-cookie';
 
 // Centralized application state
 // For more information visit http://redux.js.org/
-const initialState = { count: 0 };
+const initialState = {inventory: [], categories: [], cookieLoaded: false};
+
+const getBlankCookie = () => {
+    // Generates a cookie from a known template.
+    return {
+        date: new Date().getTime(),
+        inventory: [],
+        categories: []
+    };
+};
+
+const getCookie = ((data) => {
+    return Object.assign({}, getBlankCookie(), data);
+});
+
+const loadCookie = () => {
+    return Cookies.get(COOKIE_NAME) ? JSON.parse(Cookies.get(COOKIE_NAME)) : getBlankCookie();
+};
+
+const writeCookie = (data) => {
+    Cookies.set(COOKIE_NAME, getCookie(...data, {date: new Date().getTime()}), {expires: 7});
+};
 
 const store = createStore((state = initialState, action) => {
-  // TODO: Add action handlers (aka "reducers")
-  switch (action.type) {
-    case 'COUNT':
-      return { ...state, count: (state.count) + 1 };
-    default:
-      return state;
-  }
+
+    switch (action.type) {
+        case GET_INVENTORY:
+
+            console.log('store::action GET_INVENTORY');
+
+            var inventoryRefresh = action.refresh || false;
+
+            if (!state.cookieLoaded) {
+                var cookie = loadCookie();
+                state = {...state, inventoryLoaded: cookie.date, cookieLoaded: true};
+                inventoryRefresh = true; // Force a refresh of items.
+            }
+
+            if (inventoryRefresh) {
+                console.log('\tCookie just loaded or refresh called');
+                request
+                    .get(API + API_GATEWAYS[GET_INVENTORY])
+                    .end((err, res) => {
+                        if (err) {
+                            console.warn('GET_INVENTORY Error:', err);
+                        } else {
+                            console.log('GET_INVENTORY response:');
+                            var data = JSON.parse(res.text);
+                            if (data.response === 200) {
+                                // No error:
+                                writeCookie({inventory: data.inventory});
+                                store.dispatch({type: GET_INVENTORY_RESPONSE, data: data.inventory});
+                            } else {
+                                // Error?
+                            }
+                        }
+                    });
+            }
+            return state;
+        case GET_INVENTORY_RESPONSE:
+            console.log('store::action GET_INVENTORY_RESPONSE');
+            return {...state, inventory: action.data};
+        default:
+            return state;
+    }
 });
 
 export default store;
