188fa3cf47d5223ec1f673014adb7a179df0334f
Ferris Thomas <fsthomas@gmail.com>
2017-02-02 11:38:25 -0500

    Adds constants for cookies, the API, and loading both inventory and cookies.

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+/**
+ * Created by fst on 2/2/17.
+ */
+export const COOKIE_NAME = "com.shellybrown.linesheets";
+export const API = "http://shellybrown.com/linesheets/api/";
+export const API_GATEWAYS = {
+    action_get_inventory: "?action=get_inventory"
+};
+export const GET_INVENTORY = "action_get_inventory";
+export const GET_INVENTORY_RESPONSE = "action_get_inventory_response";
+export const LOAD_COOKIE = "action_load_cookie";
+export const LOAD_COOKIE_RESPONSE = "action_load_cookie_response";
