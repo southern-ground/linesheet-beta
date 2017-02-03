8164538d41e334d29d10edde5d419779290aa209
Ferris Thomas <fsthomas@gmail.com>
2017-02-03 11:26:49 -0500

    Adds constants for API, error handling, and new actions.

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,33 @@
 /**
  * Created by fst on 2/2/17.
  */
-export const COOKIE_NAME = "com.shellybrown.linesheets";
+
+/* API */
+
 export const API = "http://shellybrown.com/linesheets/api/";
 export const API_GATEWAYS = {
-    action_get_inventory: "?action=get_inventory"
+    action_get_inventory: "?action=get_inventory",
+    action_get_categories: "?action=get_categories",
+    action_add_category: "?action=add_category"
 };
+
+/* ERRORS */
+
+export const ERROR_CATEGORY = "invalid category name";
+
+/* ACTIONS */
+
+// Categories:
+export const ADD_CATEGORY = "action_add_category";
+export const ADD_CATEGORY_RESPONSE = "action_add_category_response";
+export const GET_CATEGORIES = "action_get_categories";
+export const GET_CATEGORIES_RESPONSE = "action_get_categories_response";
+
+// Inventory:
 export const GET_INVENTORY = "action_get_inventory";
 export const GET_INVENTORY_RESPONSE = "action_get_inventory_response";
+
+/* COOKIES */
+export const COOKIE_NAME = "com.shellybrown.linesheets";
 export const LOAD_COOKIE = "action_load_cookie";
 export const LOAD_COOKIE_RESPONSE = "action_load_cookie_response";
