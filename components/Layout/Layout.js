cac3a8a2efc31aac0045beeae64937cb0bc49ccb
Ferris Thomas <fsthomas@gmail.com>
2017-02-03 11:24:37 -0500

    Adds Blocker element for feedback during server communication.

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -8,39 +8,69 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import cx from 'classnames';
 import Header from './Header';
 import Footer from '../Footer';
 import s from './Layout.css';
+import store from '../../src/store';
 
 class Layout extends React.Component {
 
-  static propTypes = {
-    className: PropTypes.string,
-  };
+    static propTypes = {
+        className: PropTypes.string,
+    };
 
-  componentDidMount() {
-    window.componentHandler.upgradeElement(this.root);
-  }
+    constructor(props) {
+        super(props);
+        this.getBusyMessage = this.getBusyMessage.bind(this);
+        this.updateProps = this.updateProps.bind(this);
+        var appState = store.getState();
+        this.state = {busy: appState.busy, busyMsg: appState.busyMsg};
+    }
 
-  componentWillUnmount() {
-    window.componentHandler.downgradeElements(this.root);
-  }
+    componentWillMount() {
+        this.unsubscribeFunciton = store.subscribe(this.updateProps);
+    }
 
-  render() {
-    return (
-      <div className="mdl-layout mdl-js-layout" ref={node => (this.root = node)}>
-        <div className="mdl-layout__inner-container">
-          <Header />
-          <main className="mdl-layout__content">
-            <div {...this.props} className={cx(s.content, this.props.className)} />
-            <Footer />
-          </main>
-        </div>
-      </div>
-    );
-  }
+    componentDidMount() {
+        window.componentHandler.upgradeElement(this.root);
+    }
+
+    componentWillUnmount() {
+        window.componentHandler.downgradeElements(this.root);
+        this.unsubscribeFunciton();
+    }
+
+    getBusyMessage(){
+        return this.state.busyMsg == "" ? "" : <p className={s.busyMessage}>({this.state.busyMsg})</p>;
+    }
+
+    updateProps() {
+        console.log('Layout::updateProps');
+        var appState = store.getState();
+        this.setState({...this.state, busy: appState.busy, busyMsg: appState.busyMsg});
+    }
+
+    render() {
+        return (
+            <div className="mdl-layout mdl-js-layout" ref={node => (this.root = node)}>
+
+                <div className={s.blocker + " " + (this.state.busy ? "" : s.hidden)} ref="blocker">
+                    <p>Please Wait &#133;</p>
+                    {this.getBusyMessage()}
+                </div>
+
+                <div className="mdl-layout__inner-container">
+                    <Header />
+                    <main className="mdl-layout__content">
+                        <div {...this.props} className={cx(s.content, this.props.className)}/>
+                        <Footer />
+                    </main>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Layout;
